07bba9fb05b5cb9d54a5a6e3c6f6e86a6a40ae56
olivial24 <olivia.lorenz24@gmail.com>
2018-07-25 14:29:14 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -8,5 +8,48 @@ def first_steps
   sleep(0.5)
   puts "Stop"
   sleep(1)
- end
+  puts "Right foot steps right and back"
+  sleep(0.5)
+  "Left foot crosses over right"
+  sleep(0.5)
+  "Right foot steps right"
+  sleep(0.5)
+  puts "Turn"
+  sleep(1)
 end
+end
+
+def how_many_steps?
+  steps = 0 
+  loop do
+   steps += 1 
+   puts steps 
+if steps % 2 == 0 
+  puts "Left"
+else
+  puts "Right"
+  sleep(0.5)
+end 
+end 
+
+end 
+end 
+
+def break_dance
+   steps = 0 
+  loop do
+   steps += 1 
+   puts steps 
+if steps % 2 == 0 
+  puts "Left"
+else
+  puts "Right"
+  sleep(0.5) 
+  if steps == 6
+  break 
+end
+end 
+end
+
+break_dance
+
