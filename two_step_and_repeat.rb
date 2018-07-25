0534d31b99f591705cb847eec488bfe24f54106f
olivial24 <olivia.lorenz24@gmail.com>
2018-07-25 14:15:10 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -10,19 +10,3 @@ def first_steps
   sleep(1)
  end
 end
-", with sleep after each
-end
-
-def a_few_more_steps
-  # Write a loop that outputs the first two sets of steps in the Two-Step
-end
-
-def how_many_steps?
-  # Write a loop that fulfills all criteria from Part 3 of the README.md
-end
-
-def break_dance
-  # Write a solution that uses the same code as how_many_steps?, but breaks the
-  # loop if steps is equal to 6
-end
-
